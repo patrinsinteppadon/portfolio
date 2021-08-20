8c8a5b794aa23abb464dd99c42925395c4b10c2f
Patrin <patrin.sinteppadon@gmail.com>
2021-08-19 20:34:31 -0700

    abstract project cards to take arguments

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,38 +1,46 @@
 import './projectcard.css';
-import debateNowImg from './../img/debatenow2.png';
 
 /**
- * author and github are optional arguments.
+ * this component represents the container for each project 
+ * represented in the project list.
+ * @param {string} title - required
+ * @param {array} description - required. each paragraph is one string
+ * @param {object} image - required. image of the project
+ * @param {string} author - optional argument
+ * @param {string} url - optional argument
+ * @param {string} github - optional argument
  */
-function ProjectCard({title, author, description}) {
+function ProjectCard({title, description, image, author, url, github}) {
   return (
     <div className="project-card">
       <a 
         target="_blank" 
-        href="https://info340a-w20.github.io/project-kpdebate/">
+        href={url}>
         <img 
           className="card-img" 
-          src={debateNowImg} 
+          src={image} 
           alt="DebateNOW website home page" />
       </a>
+
+
       <div className="card-description">
-        <div className="left-column">
+        <div className="left-column"> {/* project title, author, and desc */}
           <h3>{title}</h3>
           {author ? <p className="author">{author}</p> : null }
-            <p style={{ marginBottom: '0px' }}>
-              {description}
-            </p>
+          {description.map(
+            (paragraph) => <p style={{marginBottom: 0}}>{paragraph}</p>
+          )}
         </div>
-        <div className="right-column">
+        <div className="right-column"> {/* action items like URL and Github */}
           <a 
             target="_blank" 
-            href="https://github.com/info340a-w20/project-kpdebate/tree/master/my-app">
+            href={github}>
             <i className="fab fa-github" />
           </a>
           <a 
             className="btn-action"
             target="_blank"
-            href="https://info340a-w20.github.io/project-kpdebate/">
+            href={url}>
               Go to website
           </a>
         </div>
--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -1,21 +1,42 @@
 import './projectlist.css';
 import ProjectCard from './ProjectCard.js';
 
+import debateNowImg from './../img/debatenow.png';
+
+const projects = [
+  {
+    title: "DebateNow",
+    image: debateNowImg,
+    author: "Co-authored with Kiran Pradhan",
+    url: "https://info340a-w20.github.io/project-kpdebate/",
+    github: "https://github.com/info340a-w20/project-kpdebate/tree/master/my-app",
+    description: [
+      "A spontaneous game about debating with your friends according to a real debate tournament format. Players select the topic, and the game guides them through the process.",
+      "More people can join the debate as audience members. The audience can vote on the winner from their own devices.",
+      "Created using ReactJS and Firebase database."
+    ]
+  }
+]
+
+/**
+ * returns a list of each project. creates one ProjectCard for
+ * each object outlined in the `projects` array.
+ */
 function ProjectList() {
   return (
     <div className="project-list">
-      <ProjectCard 
-        title="DebateNow" 
-        author="Co-authored with Kiran Pradhan"
-        // description={
-        //   <p>A spontaneous game about debating with your friends according to a real debate tournament format. Players select the topic, and the game guides them through the process.<br/><br/>
-        //   More people can join the debate as audience members. The audience can vote on the winner from their own devices.<br/><br/>
-        //   Created using ReactJS and Firebase database.</p>
-        // }
-        description="A spontaneous game about debating with your friends according to a real debate tournament format. Players select the topic, and the game guides them through the process.\n\n
-        More people can join the debate as audience members. The audience can vote on the winner from their own devices.\n\n
-        Created using ReactJS and Firebase database."
-      />
+      {
+        projects.map((project) => 
+          <ProjectCard 
+            title={project.title}
+            image={project.image}
+            author={project.author}
+            url={project.url}
+            github={project.github}
+            description={project.description}
+          />
+        )
+      }
     </div>
   );
 }
